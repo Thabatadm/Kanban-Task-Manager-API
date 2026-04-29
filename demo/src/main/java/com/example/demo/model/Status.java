15b633a060acd2bdf1e6ebfcef380e43aed2f0b9
Thatatadm <mthabatadenise@gmail.com>
2026-04-29 14:30:28 +0200

    chore: initial project configuration and test suite

--- demo/src/main/java/com/example/demo/model/Status.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model;
-
-public enum Status {
-    TO_DO,
-    IN_PROGRESS,
-    REVIEW,
-    DONE
-}
--- demo/src/main/java/com/example/demo/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProjectRepository extends JpaRepository<Project, Long>{
-    List<Project> findByMembers_User_Id(Long userId);
-}
